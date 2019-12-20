9cb60daa86ace972dced7fd93ac59ca35f039a8c
JerryFans <fanjiarong_haohao@163.com>
2019-12-20 14:25:21 +0800

    1.1.0,adapt message tups

--- lib/fastlane/plugin/apphoster/actions/apphoster_action.rb
+++ lib/fastlane/plugin/apphoster/actions/apphoster_action.rb
@@ -1,5 +1,7 @@
 require 'fastlane/action'
 require_relative '../helper/apphoster_helper'
+require 'rest-client'
+require 'json'
 
 module Fastlane
   module Actions
@@ -25,21 +27,38 @@ module Fastlane
         if file_nick_name.nil?
           file_nick_name = ""
         end
-        
-        run_script = "curl --form plat_id=36  --form token=#{token} --form file=@#{build_file} #{api_host}"
-        if file_nick_name != 0
-          run_script = "curl --form plat_id=36 --form file_nick_name=#{file_nick_name} --form token=#{token} --form file=@#{build_file} #{api_host}"  
+
+        ipa_host = params[:ipa_host]
+        if ipa_host.nil?
+          ipa_host = ""
         end
         
-        UI.message "Begin run script #{run_script}"
         UI.message "Start upload #{build_file} to #{api_host}..."
-
-        system(run_script)
-        # response = JSON.parse(info)
-        # if response['error'] != 0
-        #   UI.user_error!("Apphost Plugin Error: #{response['error']}")
-        # end
-        UI.success "Upload process finish"
+        
+        response = RestClient::Request.execute(
+          method: :post, 
+          url: url,
+          payload: {
+              token: token,
+              plat_id: plat_id,
+              :multipart => true,
+              :file => File.new(build_file, 'rb')
+          }, 
+         )
+        json = JSON.parse(response.body)
+        error = json["error"]
+        if error.nil?
+            ipa_id = json["id"]
+            name = json["name"]
+            install_url = "#{ipa_host}#{ipa_id}"
+            if ipa_host.empty?
+              UI.success "#{name} upload success install ipa_id is #{ipa_id}"
+            else
+              UI.success "#{name} upload success install url is #{install_url}"
+            end
+        else
+            UI.user_error!("upload error : #{json["error"]}")
+        end
 
       end
 
@@ -82,6 +101,11 @@ module Fastlane
                                        description: "Your file nick name",
                                           optional: true,
                                               type: String),
+          FastlaneCore::ConfigItem.new(key: :ipa_host,
+                                                env_name: "Ipa_host",
+                                             description: "ipa host to replace like xxx.com/ipa/pkgs/100 that is your install url",
+                                                optional: true,
+                                                    type: String),                                   
           FastlaneCore::ConfigItem.new(key: :ipa,
                                        env_name: "AppHost_IPA",
                                        description: "Path to your IPA file",
